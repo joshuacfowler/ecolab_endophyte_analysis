a3050b103885a43f5c9859d9dda6494a315fe0c4
joshuacfowler <jcf3@rice.edu>
2021-04-12 11:48:53 -0500

    Starting script to analyze AGHY endophyte prevalence data

---
+++ Johnson_endophyte_prevalence_analysis.R
@@ -0,0 +1,8 @@
+# Endophyte Prevalence Analysis
+# Download and analyze endophyte scores from populations of Texas grasses collected across Texas Ecolab sites
+# Authors: Ava Johnson, Ali Campbell, Josh Fowler
+# Date updated: April 12, 2021
+
+# At the start of the script, we load in R packages that we use for loading data, running statistical models, and plotting our results
+library(tidyverse)
+
